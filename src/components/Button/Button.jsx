86f4ee191e2ccbe4b409ce75f9684abed5b709a3
witcher <kravdovidova33@mail.ru>
2022-12-24 18:10:38 +0300

    commit1

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,12 @@
+import React from "react";  
+                          
+
+const Button = () => {   
+                          
+                       
+    return(
+        <button {...props} className={'button' + props.className}/>
+    );  
+};
+
+export default Button;
