ed6c3a793ba0565a07855445a01a6e8fda9b352b
witcher <kravdovidova33@mail.ru>
2022-12-24 18:34:10 +0300

    commit3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { useEffect } from 'react';
 import './App.css';
-import Header from './components/Header/Header';
-const tg = window.Telegram.WebApp;
+import { useTelegram } from "./Hooks/useTelegram";
+
 
 function App() {
-
+  const {onToggleButton, tg} = useTelegram();
+  
   useEffect(() => {
     tg.ready();
   },[])
@@ -13,7 +14,7 @@ function App() {
 
   return (
     <div className="App">
-      <Header/>
+      <button onClick={onToggleButton}>toggle</button>
     </div>
   );
 }
